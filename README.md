8975d54d48ac740018c31184ebe1ff76a3aa0a2e
yejiawei <2640199637@qq.com>
2020-12-07 22:09:41 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 test
+
+参考 https://developers.google.com/web/tools/workbox/guides/get-started
--- src/sw.js
+++ src/sw.js
@@ -1,67 +1,55 @@
 import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from "workbox-routing";
 import { NetworkFirst, StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
-
-// Used for filtering matches based on status code, header, or both
 import { CacheableResponsePlugin } from "workbox-cacheable-response";
-// Used to limit entries in cache, remove entries after a certain period of time
 import { ExpirationPlugin } from "workbox-expiration";
 
 
 precacheAndRoute(self.__WB_MANIFEST); // 传入 workbox-webpack-plugin 生成的 precache manifest 清单文件列表，用于缓存
 
-// Cache page navigations (html) with a Network First strategy
+// 使用 NetworkFirst模式 来处理导航请求(html)
+// NetworkFirst：首先请求网络资源，如果收到响应将其保存到缓存中；如果网络请求失败，从缓存中取。
 registerRoute(
-  // Check to see if the request is a navigation to a new page
-  ({ request }) => request.mode === "navigate",
-  // Use a Network First caching strategy
-  new NetworkFirst({
-    // Put all cached files in a cache named 'pages'
-    cacheName: "pages",
-    plugins: [
-      // Ensure that only requests that result in a 200 status are cached
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  })
+    ({ request }) => request.mode === "navigate", // 导航请求
+    new NetworkFirst({
+        cacheName: "pages", // 将文件缓存到名为 pages 的cache中
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200], // 确保响应状态200的文件才能缓存
+            }),
+        ],
+    })
 );
 
-// Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
+// 使用 StaleWhileRevalidate模式 来处理css，js，web worker文件
+// StaleWhileRevalidate：如果缓存中有就使用缓存的资源并且在后台默默的更新缓存中的文件，如果缓存中没有就用网络资源。缺点是会占用带宽
 registerRoute(
-  // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
-  ({ request }) => request.destination === "style" || request.destination === "script" || request.destination === "worker",
-  // Use a Stale While Revalidate caching strategy
-  new StaleWhileRevalidate({
-    // Put all cached files in a cache named 'assets'
-    cacheName: "assets",
-    plugins: [
-      // Ensure that only requests that result in a 200 status are cached
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  })
+    ({ request }) => request.destination === "style" || request.destination === "script" || request.destination === "worker",
+    new StaleWhileRevalidate({
+        cacheName: "assets", // 将文件缓存到名为 assets 的cache中
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200], // 确保响应状态200的文件才能缓存
+            }),
+        ],
+    })
 );
 
-// Cache images with a Cache First strategy
+// 使用 CacheFirst模式 来处理 image 资源
+// CacheFirst：如果缓存中有就用缓存的，如果没有就用网络的并且添加到缓存中
 registerRoute(
-  // Check to see if the request's destination is style for an image
-  ({ request }) => request.destination === "image",
-  // Use a Cache First caching strategy
-  new CacheFirst({
-    // Put all cached files in a cache named 'images'
-    cacheName: "images",
-    plugins: [
-      // Ensure that only requests that result in a 200 status are cached
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-      // Don't cache more than 50 items, and expire them after 30 days
-      new ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
-      }),
-    ],
-  })
+    ({ request }) => request.destination === "image",
+    new CacheFirst({
+        cacheName: "images",
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200], // 确保响应状态200的文件才能缓存
+            }),
+            new ExpirationPlugin({ // 不能缓存超过50个文件，有效期不能超过30天
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30天
+                purgeOnQuotaError: true, // 如果超过浏览器的存储配额将自动清除
+            }),
+        ],
+    })
 );
